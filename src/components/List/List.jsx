1a630edfb8b819f89a29f1bd59e99dd6020de99d
jpequeno96 <joaopequeno1886@gmail.com>
2022-11-29 20:06:07 +0000

    feat: ratings

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,17 +3,15 @@ import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, 
 import PlaceDetails from '../PlaceDetails/PlaceDetails'
 import useStyles from './styles';
 
-const List = ({ places, childClicked, isLoading }) => {
+const List = ({ places, childClicked, isLoading, type, setType, rating, setRating }) => {
     const classes = useStyles();
-    const [type, setType] = useState('restaurants');
-    const [rating, setRating] = useState('');
     const [elRefs, setElRefs] = useState([]);
 
     useEffect(() => {
         const refs = Array(places?.length).fill().map((_, i) => elRefs[i] || createRef());
 
         setElRefs(refs);
-    }, [places, elRefs]);
+    }, [places]);
 
     return(
         <div className={classes.container}>
